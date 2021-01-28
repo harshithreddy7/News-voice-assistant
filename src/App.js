42e1828c14672e744035d18c4ede99f8c3972020
Harshith Reddy <harshithreddy@Harshiths-MacBook-Pro.local>
2021-01-28 23:04:37 +0530

    deploy my app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import alanBtn from '@alan-ai/alan-sdk-web';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import NewsCards from "./Components/NewsCards/NewsCards";
+
+const alanKey = '0851305676e060b1d5a37e6f3342dd232e956eca572e1d8b807a3e2338fdd0dc/stage';
+
+const App = () =>{
+    const [newsArticles,setNewsArticles] =useState([]);
+    useEffect(() => {
+        alanBtn({
+            key: alanKey,
+            onCommand : ({ command , articles }) => {
+                if(command ==='newHeadlines'){
+                    setNewsArticles(articles);
+                }
+            }
+        })
+    }, [])
+    return (
+        <div>
+            <div>
+            <h1>  AI news application</h1>
+            </div>
+            <NewsCards  articles ={newsArticles} />
+        </div>
+    );
 }
 
 export default App;
