5b74cb72a7e9903f30170dd7b0155c2d84876753
Harshith Reddy <harshithreddy@Harshiths-MacBook-Pro.local>
2021-01-29 10:03:14 +0530

    Updates

---
+++ precache-manifest.fa730946d22794332931e9b68a94db13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "443e1877892012d3cd2144cf1db09687",
+    "url": "/News-voice-assistant/index.html"
+  },
+  {
+    "revision": "8c3776d8d61609e60c86",
+    "url": "/News-voice-assistant/static/css/main.7183bf1f.chunk.css"
+  },
+  {
+    "revision": "bf2d4ac8f75a319bc5c2",
+    "url": "/News-voice-assistant/static/js/2.4adbccbf.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/News-voice-assistant/static/js/2.4adbccbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c3776d8d61609e60c86",
+    "url": "/News-voice-assistant/static/js/main.f7f3a8e3.chunk.js"
+  },
+  {
+    "revision": "5ff6e7ded55b1e8fa8e7",
+    "url": "/News-voice-assistant/static/js/runtime-main.43e828e3.js"
+  }
+]);
